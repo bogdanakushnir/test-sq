a6d8023f2758bdc3590c98ddfcac950ceae57d34
Bogdana <bogdana.kushnir@sonarsource.com>
2024-08-26 17:05:22 +0200

    init

---
+++ src/main/java/org/test/example/Main1.java
@@ -0,0 +1,53 @@
+package org.test.example;
+
+public class Main1 {
+  public static void main(String[] args) throws Exception{
+    long j =02343533;
+    String s;
+    System.out.println("Hello world!");
+    int i = 0;
+    boolean f;
+
+    if (i==0){
+      f = true;
+    }
+
+    String p;
+    String p1 = getPassword();
+    System.out.println(p1);
+  }
+
+  private static String getPassword(){
+    String password = "askjdhk";
+    return password;
+  }
+
+
+  private static String getPassword2(){
+    String password = "askjdhk";
+    return password;
+  }
+
+  private static String smthngNew2(){
+    String password = "askjdhk";
+    String password1 = "askjdhk";
+    String password2 = "askjdhk";
+    String password3 = "askjdhk";
+    return password;
+  }
+
+  private static String empty(){
+
+    return null;
+  }
+//  private static String empty2(){
+//    String password = "askjdhk";
+//    return password;
+//  }  private static String empty3(){
+//    String password = "askjdhk";
+//    return password;
+//  }  private static String empty4(){
+//    String password = "askjdhk";
+//    return password;
+//  }
+}
